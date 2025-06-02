f0eb7dfea2468dd98c0e7f7c514af09e6af3307f
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-06-03 01:20:34 +0600

    add

--- src/Component/Pages/Inicio.jsx
+++ src/Component/Pages/Inicio.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Inicio = () => {
   return (
     <div>
-        <h1 className='text-3xl font-bold text-center mt-10'>Inicio</h1>
+        <h1 className='text-3xl font-bold text-center'>Inicio</h1>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel accusamus fuga, temporibus, modi doloribus nihil vitae dolores quo qui asperiores officiis. Necessitatibus, autem. Ab porro quam, corporis quis expedita maiores iure impedit distinctio error vitae nostrum, labore voluptatum officiis asperiores architecto hic? Iusto, facilis veritatis sit molestias nobis doloribus aut esse, nemo delectus harum obcaecati cumque dolore quidem facere! Quisquam quod, quos fugit saepe eaque sint consectetur aperiam aliquam accusantium eos aspernatur dolorem quia culpa necessitatibus architecto reiciendis expedita eius, earum labore nihil tempora. Possimus impedit dolore voluptas fugiat. Non.
     </div>
   )
